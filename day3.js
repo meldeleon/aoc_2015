458f76556700e2882bd77ef3be001e71c9c528f4
meldeleon <melissadeleon@gmail.com>
2021-04-06 22:27:00 -0700

    solving day 3 pt 1

---
+++ day3.js
@@ -0,0 +1,33 @@
+//day 3 pt 1
+const input = Array.from(
+  require("fs").readFileSync("day3_input.txt").toString()
+)
+console.log(input)
+
+let santa = [0, 0]
+
+let houseArray = input.map((direction) => {
+  let [x, y] = santa
+  if (direction === "^") {
+    santa = [x, y + 1]
+  } else if (direction === ">") {
+    santa = [x + 1, y]
+  } else if (direction === "v") {
+    santa = [x, y - 1]
+  } else if (direction === "<") {
+    santa = [x - 1, y]
+  } else {
+    console.log("invalid input: " + direction)
+  }
+  return santa
+})
+houseArray.unshift([0, 0])
+
+let stringHouseArray = houseArray.map((house) => {
+  return house.toString()
+})
+
+let uniqueHouseArray = new Set(stringHouseArray)
+console.log(uniqueHouseArray)
+
+console.log(uniqueHouseArray.size)
