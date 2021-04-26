cf0cd2b946e85211f3f6720e6fbbbac1e1addd8a
meldeleon <melissadeleon@gmail.com>
2021-04-25 18:32:46 -0700

    completed challenge for small input, big input may need a refactor

--- day7.js
+++ day7.js
@@ -8,37 +8,76 @@ const input = require("fs")
 let queue = [...input]
 let board = {}
 
-/*number  -> wire
-wire OPERATOR wire -> wire
-wire OPERATOR number -> wire
-NOT wire -> NOT
-*/
+runOperations(queue)
+Object.keys(board).forEach((x) => {
+  board[x] = bitToInt(board[x])
+})
+console.log(board)
+console.log(board[a])
 
-
+function runOperations(instructions) {
+  while (queue.length > 0) {
+    queue.forEach((x, index) => {
+      if (parseCommand(x) === null) {
+        return
+      } else {
+        parseCommand(x)
+        queue.splice(index, 1)
+      }
+    })
+  }
+}
 function parseCommand(command) {
-  let cmdArray = command.split(" ")
-  if (cmdArray[0] === not) {
-    //complement
-  } else if (cmdArray[1] === "->") {
-    // basic assignment
+  let [instruction, destinationWire] = command.split(" -> ")
+  instructionArray = instruction.split(" ")
+  //console.log(instructionArray, destinationWire)
+  if (instructionArray[0] === "NOT") {
+    if (board[instructionArray[1]]) {
+      board[destinationWire] = bitwiseComplement(board[instructionArray[1]])
+    } else {
+      // if left-hand wires not defined, punt
+      return null
+    }
+  } else if (instructionArray.length === 1) {
+    board[destinationWire] = intTo16Bit(instructionArray[0])
   } else {
-    if ()
+    let [operand1, operator, operand2] = instructionArray
+    switch (operator) {
+      case "OR":
+        if (board[operand1] && board[operand2]) {
+          board[destinationWire] = bitwiseOr(board[operand1], board[operand2])
+        } else {
+          return null
+        }
+        break
+      case "AND":
+        if (board[operand1] && board[operand2]) {
+          board[destinationWire] = bitwiseAnd(board[operand1], board[operand2])
+        } else {
+          return null
+        }
+        break
+      case "LSHIFT":
+        if (board[operand1]) {
+          board[destinationWire] = bitwiseLShift(board[operand1], operand2)
+        } else {
+          return null
+        }
+        break
+      case "RSHIFT":
+        if (board[operand1]) {
+          board[destinationWire] = bitwiseRShift(board[operand1], operand2)
+        } else {
+          return null
+        }
+        break
+    }
   }
 }
 
-// let basicAssignments = input.filter((x) => x.match(/(^\d* ->)/g))
-// basicAssignments.forEach((x) => {
-//   board[x.match(/.$/g).toString()] = intTo16Bit(x.match(/.$/g))
-// })
-// console.log(board)
-// // iterate over the known wires, and find assignments that use only a known value.
-// Object.entries(board).forEach((x) => {})
-
-
-
 function bitwiseAnd(arr1, arr2) {
-  let y = arr2[index]
   return arr1.map((x, index) => {
+    let y = arr2[index]
     return x * y
   })
 }
@@ -55,6 +94,7 @@ function bitwiseOr(arr1, arr2) {
 }
 
 function bitwiseRShift(arr, shiftNumber) {
+  shiftNumber = parseInt(shiftNumber)
   let shiftedArr = [...arr]
   for (let i = 0; i < shiftNumber; i++) {
     shiftedArr.unshift(0)
@@ -64,10 +104,11 @@ function bitwiseRShift(arr, shiftNumber) {
 }
 
 function bitwiseLShift(arr, shiftNumber) {
+  shiftNumber = parseInt(shiftNumber)
   let shiftedArr = [...arr]
   for (let i = 0; i < shiftNumber; i++) {
-    shiftedArr.push(0)
     shiftedArr.shift()
+    shiftedArr.push(0)
   }
   return shiftedArr
 }
@@ -90,4 +131,14 @@ function intTo16Bit(int) {
     int = int % power
   }
   return bitArray
+}
+
+function bitToInt(arr) {
+  let int = 0
+  arr.forEach((x, index) => {
+    if (x === 1) {
+      int += 2 ** (15 - index)
+    }
+  })
+  return int
 }
