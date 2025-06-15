bd44316fb9690c527ac14b6e74bdc3b14ebb061f
Melissa deLeon <melissadeleon@gmail.com>
2025-06-14 23:28:24 -0400

    added cache for day 7; solved part 1

--- day07_a.js
+++ day07_a.js
@@ -1,15 +1,20 @@
-//define target wire here
-const promptWire = "f"
+//define target wire 
+const targetWire = "a"
+
 //turning input into an arr
 const data = require("fs")
   .readFileSync("day07_input.txt")
   .toString()
   .split("\n")
 
-const targetWire = "a"
+
+
 
 //turn input into lines with parts for executing a bitwise operation
 const mappings = new Object()
+
+//create cach so we don't have to recurse so much
+const cache = new Object()
 
 data.forEach(line => {
     let lineArr = line.split(" ")
@@ -44,7 +49,6 @@ data.forEach(line => {
 
 })
 
-console.log(mappings)
 let solution = findWireValue(targetWire)
 console.log(`The answer is ${solution}`)
 
@@ -52,33 +56,43 @@ console.log(`The answer is ${solution}`)
 
 // //recursive function to decide what the value of a wire
 function findWireValue(wire){
-    //pull mapping
-    let {type, inputs} = mappings[wire]
-    console.log(type, inputs)
-    //iterate over inputs
-    let solvedInputs = inputs.map(input => {
-        //if the inputs are all known values, return them in the map
-        if (parseInt(input) == input) return parseInt(input)
-        //in there
-        else {
-            console.log(`recursing to find ${input}`)
-            return findWireValue(input) 
-        }
-    })
-    return executeBitwise(type, solvedInputs)
-    
+    console.log(`looking for wire ${wire}`)
+    //check if wire is cached
+    if (cache[wire]){
+        console.log(`found in cache with value of ${cache[wire]}`)
+        return cache[wire]
+    } else {
+        //pull mapping
+        let {type, inputs} = mappings[wire]
+        console.log(type, inputs)
+        //iterate over inputs
+        let solvedInputs = inputs.map(input => {
+            //if the inputs are all known values, return them in the map
+
+            if (parseInt(input) == input) return parseInt(input)
+            else {
+                //check to see if value is cached
+                console.log(`recursing to find ${input}`)
+                return findWireValue(input) 
+            }
+        })
+        console.log(`inputs solved: ${solvedInputs}, ${type}`)
+        let value = executeBitwise(type, solvedInputs)
+        cache[wire] = value
+        return value
+    }
 }
 
 function executeBitwise(type, inputs){
     switch(type){
         case "ASSIGN":
-            return inputs[0]
+            return inputs[0] & 0xffff
         case "NOT":
             return ~inputs[0] & 0xffff
         case "AND":
-            return (inputs[0] && inputs[1]) & 0xffff 
+            return (inputs[0] & inputs[1]) & 0xffff 
         case "OR":
-            return (inputs[0] || inputs[1]) & 0xffff
+            return (inputs[0] | inputs[1]) & 0xffff
         case "LSHIFT":
             return (inputs[0] << inputs[1]) & 0xffff
         case "RSHIFT":
